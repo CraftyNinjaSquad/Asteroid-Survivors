3fdbe9bdf831dbf261ac7be11ad88f4525cf4da6
CraftyNinjaSquad <ngardner4843@gmail.com>
2026-06-25 22:03:00 -0700

    added asteroid texture/behavior

--- Scripts/ohre.gd
+++ Scripts/ohre.gd
@@ -1,6 +1,8 @@
 extends Area3D
 
+var value = globals.currency_mult
+
 func _on_area_entered(area: Area3D) -> void:
 	if area.is_in_group("Player"):
-		globals.currency += globals.currency_mult
+		globals.currency += value
 		queue_free()
